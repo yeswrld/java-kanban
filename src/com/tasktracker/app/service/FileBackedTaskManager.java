3f2249329ac9232ca2d09503bead2c303e0a8702
Damir Minigaliev <dminigaliev@gmail.com>
2024-10-20 22:49:23 +0500

    первая попытка сдать 7 спринт.

---
+++ src/com/tasktracker/app/service/FileBackedTaskManager.java
@@ -0,0 +1,194 @@
+package com.tasktracker.app.service;
+
+import com.tasktracker.app.model.*;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class FileBackedTaskManager extends InMemoryTaskManager {
+    private final File file;
+
+    public FileBackedTaskManager(HistoryManager historyManager, File file) {
+        super(historyManager);
+        this.file = file;
+    }
+
+    public static FileBackedTaskManager load(HistoryManager historyManager, File file) {
+        FileBackedTaskManager fileBackedTaskManager = new FileBackedTaskManager(historyManager, file);
+        fileBackedTaskManager.load(file);
+        return fileBackedTaskManager;
+    }
+
+    @Override
+    public Task addTaskM(Task task) {
+        super.addTaskM(task);
+        save();
+        return task;
+    }
+
+    @Override
+    public Epic addEpicM(Epic epic) {
+        super.addEpicM(epic);
+        save();
+        return epic;
+    }
+
+    @Override
+    public Subtask addSubTaskM(Subtask subtask) {
+        super.addSubTaskM(subtask);
+        save();
+        return subtask;
+    }
+
+    @Override
+    public void updateEpic(Epic epic) {
+        super.updateEpic(epic);
+        save();
+    }
+
+    @Override
+    public void updateSubstask(Subtask subtask) {
+        super.updateSubstask(subtask);
+        save();
+    }
+
+    @Override
+    public void updateTask(Task task) {
+        super.updateTask(task);
+        save();
+    }
+
+    @Override
+    public void deleteEpics() {
+        super.deleteEpics();
+        save();
+    }
+
+    @Override
+    public void deleteTasks() {
+        super.deleteTasks();
+        save();
+    }
+
+    @Override
+    public void deleteSubtasks() {
+        super.deleteSubtasks();
+        save();
+    }
+
+    @Override
+    public void removeEpicOnId(int id) {
+        super.removeEpicOnId(id);
+        save();
+    }
+
+    @Override
+    public void removeSubTaskOnId(int id) {
+        super.removeSubTaskOnId(id);
+        save();
+    }
+
+    @Override
+    public void removeTaskOnId(int id) {
+        super.removeTaskOnId(id);
+        save();
+    }
+
+    private void save() {
+        String head = "id,type,name,status,description,epic,\n";
+        try {
+            Writer fileWriter = new FileWriter(file, StandardCharsets.UTF_8, false);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            {
+                bufferedWriter.write(head);
+                for (Task task : getTasks()) {
+                    bufferedWriter.write(toString(task));
+                }
+                for (Task task : getEpics()) {
+                    bufferedWriter.write(toString(task));
+                }
+                for (Task task : getSubtasks()) {
+                    bufferedWriter.write(toString(task));
+                }
+                bufferedWriter.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void load(File file) {
+        try (Reader reader = new FileReader(file, StandardCharsets.UTF_8); BufferedReader bufferedReader = new BufferedReader(reader)) {
+            bufferedReader.readLine();
+            while (bufferedReader.ready()) {
+                String line = bufferedReader.readLine();
+                if (line.isEmpty()) continue;
+                Task task = stringToTask(line);
+                if (counter < task.getId()) {
+                    counter = task.getId();
+                }
+                if (task.getClass().equals(Task.class)) {
+                    taskM.put(task.getId(), task);
+                } else if (task.getClass().equals(Epic.class)) {
+                    Epic epic = (Epic) task;
+                    epicM.put(epic.getId(), epic);
+                } else if (task.getClass().equals(Subtask.class)) {
+                    Subtask subtask = (Subtask) task;
+                    subTaskM.put(subtask.getId(), subtask);
+                    Epic epic = epicM.get(subtask.getEpicId());
+                    epic.addSubtaskId(subtask.getId());
+                }
+            }
+
+            if (counter != 1) counter++;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String toString(Task task) {
+        String epicId = "";
+        if (task.getType().equals(Type.SUBTASK)) {
+            epicId = String.valueOf(((Subtask) task).getEpicId());
+        }
+        return task.getId() + "," +
+                task.getType() + "," +
+                task.getName() + "," +
+                task.getStatus() + "," +
+                task.getDescription() + "," +
+                epicId + "\n";
+
+    }
+
+    private static Task stringToTask(String value) {
+        String[] split = value.split(",");
+        int id = Integer.parseInt(split[0]);
+        Type tasktype = Type.valueOf(split[1]);
+        String name = split[2];
+        Status status = Status.valueOf(split[3]);
+        String description = split[4];
+        int epicId = 0;
+        if (tasktype.equals(Type.SUBTASK)) {
+            epicId = Integer.parseInt(split[5]);
+        }
+
+        if (tasktype == Type.TASK) {
+            Task task = new Task(name, description, status);
+            task.setId(id);
+            task.setStatus(status);
+            return task;
+        } else if (tasktype == Type.EPIC) {
+            Epic epic = new Epic(name, description);
+            epic.setId(id);
+            epic.setStatus(status);
+            return epic;
+        } else {
+            Subtask subtask = new Subtask(name, description, status, epicId);
+            subtask.setId(id);
+            subtask.setStatus(status);
+            return subtask;
+
+        }
+    }
+
+}
---
+++ test/service/FileBackedTaskManagerTest.java
@@ -0,0 +1,87 @@
+package service;
+
+import com.tasktracker.app.model.*;
+import com.tasktracker.app.service.FileBackedTaskManager;
+import com.tasktracker.app.service.Managers;
+import com.tasktracker.app.service.TaskManager;
+import org.junit.jupiter.api.*;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FileBackedTaskManagerTest {
+    private TaskManager taskManager, taskManagerLoad;
+    private File file;
+
+
+    @BeforeEach
+    public void beforeEach() throws IOException {
+        file = File.createTempFile("storage", ".csv");
+        taskManager = FileBackedTaskManager.load(Managers.getDefaultHistory(), file);
+        Task task1 = new Task("Задача 1", "Описание задачи 1", Status.NEW);
+        taskManager.addTaskM(task1);
+        Epic epic1 = new Epic("Эпик 1", "Описание эпика 1");
+        taskManager.addEpicM(epic1);
+        Subtask subtask2 = new Subtask("Подзадача 2", "Описание подзадачи 2", Status.IN_PROGRESS, 3);
+        taskManager.addSubTaskM(subtask2);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        file.deleteOnExit();
+    }
+
+    @DisplayName("Создание и сохранение в файл")
+    @Test
+    void createAndSave() throws IOException {
+
+        List<String> linesList = new ArrayList<>();
+        try (Reader reader = new FileReader(file, StandardCharsets.UTF_8);
+             BufferedReader br = new BufferedReader(reader)) {
+            while (br.ready()) {
+                String line = br.readLine();
+                linesList.add(line + "\n");
+            }
+        }
+        Assertions.assertEquals(linesList.get(0), "id,type,name,status,description,epic,\n");
+        Assertions.assertEquals(linesList.get(1), "2,TASK,Задача 1,NEW,Описание задачи 1,\n");
+        Assertions.assertEquals(linesList.get(3), "4,SUBTASK,Подзадача 2,IN_PROGRESS,Описание подзадачи 2,3\n");
+    }
+
+    @DisplayName("Очистка Менеджера задач и проверка пустоты хранилища")
+    @Test
+    void cleaningTaskManager() { // после удаления задач всех типов в истории должна остаться только одна строка - оглавление
+        taskManager.deleteTasks();
+        taskManager.deleteSubtasks();
+        taskManager.deleteEpics();
+        List<String> linesList = new ArrayList<>();
+        try (Reader reader = new FileReader(file, StandardCharsets.UTF_8);
+             BufferedReader br = new BufferedReader(reader)) {
+            while (br.ready()) {
+                String line = br.readLine();
+                linesList.add(line + "\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Assertions.assertEquals(linesList.get(0), "id,type,name,status,description,epic,\n");
+        Assertions.assertTrue(linesList.size() == 1);
+    }
+
+    @DisplayName("Загрузка из файла")
+    @Test
+    void loadFromFile() {
+        taskManagerLoad = FileBackedTaskManager.load(Managers.getDefaultHistory(), file);
+        List<Task> tasks = new ArrayList<>(taskManagerLoad.getTasks());
+        List<Epic> epics = new ArrayList<>((Collection) taskManagerLoad.getEpics());
+        List<Subtask> subtasks = new ArrayList<>((Collection) taskManagerLoad.getSubtasks());
+        Assertions.assertEquals(tasks.get(0).getName() , "Задача 1");
+        Assertions.assertEquals(epics.get(0).getType() , Type.EPIC);
+        Assertions.assertEquals(subtasks.get(0).getDescription(), "Описание подзадачи 2");
+
+    }
+
+}
